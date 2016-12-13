a99afc57fb6d513e5a13b98b7a22dbb8a658c346
Johan Carlsson <carlsson@pppl.gov>
2016-12-13 15:23:47 -0500

    core_profiles data in global_quantities and profiles_1d containers can be read from IMAS tree successfully

--- imas2transp/imas-cp-test.py
+++ imas2transp/imas-cp-test.py
@@ -29,5 +29,18 @@ print 'core_profiles%time(-1) =', ids.time
 
 #print dir(ids.global_quantities)
 print 'core_profiles%global_quantities%v_loop(-1) =', ids.global_quantities.v_loop
+print 'core_profiles%global_quantities%li(-1) =', ids.global_quantities.li
+print 'core_profiles%global_quantities%beta_pol(-1) =', ids.global_quantities.beta_pol
+
+#print dir(ids.profiles_1d[-1])
+print 'core_profiles%profiles_1d(-1)%t_i_average =', ids.profiles_1d[-1].t_i_average
+print 'core_profiles%profiles_1d(-1)%n_i_total_over_n_e =', ids.profiles_1d[-1].n_i_total_over_n_e
+print 'core_profiles%profiles_1d(-1)%zeff =', ids.profiles_1d[-1].zeff
+print 'core_profiles%profiles_1d(-1)%j_tor =', ids.profiles_1d[-1].j_tor
+print 'core_profiles%profiles_1d(-1)%j_ohmic =', ids.profiles_1d[-1].j_ohmic
+print 'core_profiles%profiles_1d(-1)%j_bootstrap =', ids.profiles_1d[-1].j_bootstrap
+print 'core_profiles%profiles_1d(-1)%q =', ids.profiles_1d[-1].q
+print 'core_profiles%profiles_1d(-1)%magnetic_shear =', ids.profiles_1d[-1].magnetic_shear
+print 'core_profiles%profiles_1d(-1)%time =', ids.profiles_1d[-1].time
 
 itm.close()
